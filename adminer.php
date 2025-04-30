f3fba4e18c40b560fb8e3eed10ee351f707976fe
ninilee1123 <wlsdl9010@gmail.com>
2025-04-30 16:04:29 +0900

    Update adminer.php

--- adminer.php
+++ adminer.php
@@ -1,18 +1,3 @@
 <?php
-// adminer.php - 웹에서 DB 관리 도구
-// 원본 출처: https://www.adminer.org/latest.php
-
-function adminer_object() {
-  include_once "./editor-plugins/plugin.php";
-
-  class AdminerSoftware extends Adminer {
-    function name() {
-      return '일정관리 DB';
-    }
-  }
-
-  return new AdminerSoftware;
-}
-
-include "./editor-plugins/adminer.php";
+include "https://www.adminer.org/latest.php";
 ?>
