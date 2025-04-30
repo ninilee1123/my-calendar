12f588a943939070d01b9f42cbe242b63d5e0214
ninilee1123 <wlsdl9010@gmail.com>
2025-04-30 16:23:46 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $host = "sql12.freesqldatabase.com";
 $db_user = "sql12776136";      // 회원가입 시 입력한 사용자 이름
-$db_pass = "1308503";    // 회원가입 시 입력한 비밀번호
+$db_pass = "R2jJ6nh555";    // 회원가입 시 입력한 비밀번호
 $db_name = "sql12776136";  // 회원가입 시 입력한 데이터베이스 이름
 
 $conn = new mysqli($host, $db_user, $db_pass, $db_name);
