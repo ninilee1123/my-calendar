53afa591e3e48c857a9855570bfd3352131a438d
ninilee1123 <wlsdl9010@gmail.com>
2025-04-30 17:07:31 +0900

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $host = "sql305.infinityfree.com";
 $db_user = "if0_38868400";      // 회원가입 시 입력한 사용자 이름
 $db_pass = "b9f97MTTk87w";    // 회원가입 시 입력한 비밀번호
-$db_name = "if0_38868400_calendar ";  // 회원가입 시 입력한 데이터베이스 이름
+$db_name = "if0_38868400_calendar";  // 회원가입 시 입력한 데이터베이스 이름
 
 $conn = new mysqli($host, $db_user, $db_pass, $db_name);
 $conn->set_charset("utf8");
